d1f5ac2c322e5eb0729d6891deb1916c5b70f8ca
cemalkuller <cemalkuller@gmail.com>
2023-06-05 05:54:23 +0300

    ...

--- app/types.tsx
+++ app/types.tsx
@@ -1,4 +1,7 @@
+import { DrawerActionType } from "@react-navigation/native";
+
 export type Navigation = {
+  dispatch(arg0: DrawerActionType): unknown;
   navigate: (scene: string) => void;
   toggleDrawer: (action: void) => void;
   navigation : void ;
